b39c0ba249ff3745b3a3ac2668acf29ba13b560c
KruMot <duangkamol.t@tpp.ac.th>
2025-04-09 00:05:22 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import questionsData from "../src/data/questions";
+import questionsData from "./data/questions";
 // ...
 
 // (โค้ด App.jsx ทั้งหมดตามที่แก้ไว้จะใส่ในนี้)
